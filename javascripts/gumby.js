cc4d695adba62cdf425d0849b99ea152a61fd74a
Paul Ledbetter <ledbetter.paul+github@gmail.com>
2014-05-28 21:36:30 +0000

    Fix val id.

--- javascripts/gumby.js
+++ javascripts/gumby.js
@@ -3,7 +3,7 @@
 $(function () {
     $('button').click(function () {
         console.log({
-            url: $('#target').val() + '?' + encodeURIComponent($('#key').val()) + '=' + encodeURIComponent($('#val').val())
+            url: $('#target').val() + '?' + encodeURIComponent($('#key').val()) + '=' + encodeURIComponent($('#value').val())
             , type: 'PUT'
         });
         // $.ajax({
