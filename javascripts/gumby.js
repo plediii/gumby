712ae9d6a41e26b7b02174a1cf4c2e8756652942
Paul Ledbetter <ledbetter.paul+github@gmail.com>
2014-05-28 22:13:48 +0000

    Store target and key in local storage.

--- javascripts/gumby.js
+++ javascripts/gumby.js
@@ -1,6 +1,39 @@
 
 
 $(function () {
+    var session = {
+        set: function (name, val) {
+            if (typeof(Storage) !== "undefined")
+            {
+                sessionStorage[name] = val;
+            }
+        }
+        , get: function (name) {
+            if (typeof(Storage) !== "undefined")
+            {
+                return sessionStorage[name];
+            }
+            else {
+                console.log('no local storage');
+                return void 0;
+            }
+        }
+    };
+
+    var remember = function (field) {
+        $('#' + field).val(session.get(field));
+        $('button').click(function () {
+            session.set(field, $('#' + field).val());
+        });
+    };
+
+    remember('target');
+    remember('key');
+
+    if (session.get('target')) {
+        $('#target').val(session.get('target'));
+    }
+
     $('button').click(function () {
         $.ajax({
             url: $('#target').val() + '?' + encodeURIComponent($('#key').val()) + '=' + encodeURIComponent($('#value').val())
